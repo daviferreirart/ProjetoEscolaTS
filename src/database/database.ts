62bed52dee8f76c88d1bbacd05dee9085c20300b
Davi Ferreira <daviferreirart@gmail.com>
2021-02-10 16:22:49 -0300

    Adicionando extensões do vscode

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,16 +1,17 @@
-import mysql, { Connection } from "mysql2/promise";
+import mysql, { Connection } from 'mysql2/promise';
 
 abstract class Database {
-    static connection: Connection
+    static connection: Connection;
+
     static async connect() {
         this.connection = await mysql.createConnection({
             host: 'localhost',
             port: 3306,
             user: 'root',
             password: 'DAVI890',
-            database: 'escola'
-        })
-        console.log("Conectado ao banco!")
+            database: 'escola',
+        });
+        console.log('Conectado ao banco!');
     }
 }
 export default Database;
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,3 @@
 import Database from '../database/database';
 
-Database.connect()
+Database.connect();
